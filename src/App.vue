b83bb8a6881178db95a7b50eb4b9d9306c63525a
fykri <dzulfikriyunus@gmail.com>
2024-05-06 23:36:01 +0800

    responsive design

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Todos from './components/Todos.vue'
 
 <template>
   <div class="h-screen  flex items-center justify-center bg-blue-100">
-    <div class="w-1/2 rounded-md shadow-lg flex justify-center items-center flex-col bg-white pb-5">
+    <div class="w-96 rounded-md shadow-lg flex justify-center items-center flex-col bg-white pb-5 md:w-1/2  sm:w-96">
       <h1 class="text-xl font-bold p-3">TODO LIST</h1>
       <Todos/>
     </div>
